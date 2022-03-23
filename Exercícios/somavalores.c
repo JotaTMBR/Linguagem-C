32471ca1bff3cf0a2bc2f6ec7c49712a2139b6dd
Joao <joaosema.alves123@gmail.com>
2022-03-23 11:44:01 -0300

    Exercicios em C

---
+++ Exercícios/somavalores.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(){
+    int soma, n1, n2;
+    
+    printf("Informe o primeiro numero:");
+    scanf("%d", &n1);
+    printf("Informe o segundo numero:");
+    scanf("%d", &n2);
+
+    soma = n1 + n2;
+
+    printf("O resultado da soma é: %d\n", soma);
+
+}
